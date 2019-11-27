d21355d967059f87707a95908822c32f110de842
BastienS1A <brbio9thique@netcourrier.com>
2019-11-27 23:48:36 +0100

    Une des gouttes d'eau subit une accélération lié à gravité

---
+++ Bureau/Assets/Scripts/Eau.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eau : MonoBehaviour
+{
+
+    public GameObject Sphere;
+    private Vector3 chute=new Vector3(0, -0.0981f, 0);
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Sphere.transform.position + chute;
+        chute=new Vector3(0, -0.981f*Time.time, 0);
+        }
+}
